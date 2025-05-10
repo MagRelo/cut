8d6c940b7270e09889657a755e0a257217d13c15
Matt Lovan <matt@rara.social>
2025-05-10 16:29:43 -0400

    integrate into posthog into react

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
+import { PostHogProvider } from 'posthog-js/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_POSTHOG_KEY}
+      options={{
+        api_host:
+          import.meta.env.VITE_POSTHOG_HOST || 'https://app.posthog.com',
+        loaded: (posthog) => {
+          if (import.meta.env.DEV) posthog.debug();
+        },
+        capture_pageview: true,
+        capture_pageleave: true,
+        autocapture: true,
+      }}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>
 );
