85fa27d4355b723bee8f9e90fd122615e6a227a5
Matt Lovan <matt@rara.social>
2025-05-11 10:22:46 -0400

    refactor PostHog integration to conditionally render based on production environment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,21 +4,27 @@ import { App } from './App';
 import './index.css';
 import { PostHogProvider } from 'posthog-js/react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const app = (
   <React.StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_POSTHOG_KEY}
-      options={{
-        api_host:
-          import.meta.env.VITE_POSTHOG_HOST || 'https://app.posthog.com',
-        loaded: (posthog) => {
-          if (import.meta.env.DEV) posthog.debug();
-        },
-        capture_pageview: true,
-        capture_pageleave: true,
-        autocapture: true,
-      }}>
+    {import.meta.env.PROD ? (
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_POSTHOG_KEY}
+        options={{
+          api_host:
+            import.meta.env.VITE_POSTHOG_HOST || 'https://app.posthog.com',
+          loaded: (posthog) => {
+            if (import.meta.env.DEV) posthog.debug();
+          },
+          capture_pageview: true,
+          capture_pageleave: true,
+          autocapture: true,
+        }}>
+        <App />
+      </PostHogProvider>
+    ) : (
       <App />
-    </PostHogProvider>
+    )}
   </React.StrictMode>
 );
+
+ReactDOM.createRoot(document.getElementById('root')!).render(app);
