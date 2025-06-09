e8355274556a67e0738d071496866d46254d40d6
Matt Lovan <matt@rara.social>
2025-06-09 00:09:39 -0400

    Update PortoPage to enhance account display and connection logic
    - Changed page header from 'Porto' to 'Account' for better context.
    - Added detailed account information display including address, chain name, and chain ID.
    - Improved connection logic to conditionally render connect/disconnect buttons based on user authentication status.
    - Removed unused status display and streamlined error handling.

--- client/src/pages/PortoPage.tsx
+++ client/src/pages/PortoPage.tsx
@@ -3,43 +3,52 @@ import { PageHeader } from '../components/util/PageHeader';
 
 export function PortoPage() {
   const { address, chainId, chain } = useAccount();
-  const { connectors, connect, status, error } = useConnect();
+  const { connectors, connect, error } = useConnect();
   const { disconnect } = useDisconnect();
 
   return (
     <div className='p-4'>
-      <PageHeader title='Porto' />
+      <PageHeader title='Account' />
+      <div className='grid grid-cols-[85px_1fr] gap-2 mt-4'>
+        <div className='font-medium'>Address:</div>
+        <div>
+          {address?.slice(0, 6)}...{address?.slice(-4)}
+        </div>
 
-      <div>
-        <h2 className='text-2xl font-bold mb-4'>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded disabled:opacity-50'
-            disabled={!!address}
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type='button'>
-            {connector.name}
-          </button>
-        ))}
+        <div className='font-medium'>Chain:</div>
+        <div>{chain?.name}</div>
 
-        <button
-          className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded disabled:opacity-50'
-          disabled={!address}
-          onClick={() => disconnect()}>
-          Sign out
-        </button>
-        <div> status: {status}</div>
-        <div>{error?.message}</div>
+        <div className='font-medium'>Chain ID:</div>
+        <div>{chainId}</div>
       </div>
 
-      <div>
-        <h2 className='text-2xl font-bold mb-4'>Account</h2>
-        <div>chain: {chain?.name}</div>
-        <div>chainId: {chainId}</div>
-        <div>
-          address: {address?.slice(0, 6)}...{address?.slice(-4)}
-        </div>
+      <div className='flex flex-col gap-4 mt-4'>
+        {!address && (
+          <>
+            {connectors.map((connector) => (
+              <button
+                className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded disabled:opacity-50'
+                disabled={!!address}
+                key={connector.uid}
+                onClick={() => connect({ connector })}
+                type='button'>
+                Connect
+              </button>
+            ))}
+          </>
+        )}
+
+        {!!address && (
+          <button
+            className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded disabled:opacity-50'
+            disabled={!address}
+            onClick={() => disconnect()}>
+            Sign out
+          </button>
+        )}
+
+        {/* <div> status: {status}</div> */}
+        <div>{error?.message}</div>
       </div>
     </div>
   );
