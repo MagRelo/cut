729249fedfb82468ba4bac5b0f56553576555eea
Matt Lovan <matt@rara.social>
2025-06-09 11:11:53 -0400

    Enhance PortoPage layout with detailed account and rewards information
    - Added balance display and deposit button (disabled) for user account.
    - Introduced rewards section showing refunds and referrals credits.
    - Improved overall layout with consistent styling and spacing for better user experience.

--- client/src/pages/PortoPage.tsx
+++ client/src/pages/PortoPage.tsx
@@ -10,7 +10,39 @@ export function PortoPage() {
     <div className='p-4'>
       <PageHeader title='Account' className='mb-3' />
 
-      <div className='bg-white rounded-lg shadow p-4'>
+      <div className='bg-white rounded-lg shadow p-4 mb-4'>
+        <div className='grid grid-cols-[85px_1fr] gap-2'>
+          <div className='font-medium'>Balance:</div>
+          <div>892 credits</div>
+        </div>
+
+        <div className='mt-4 flex flex-col gap-2'>
+          <button
+            className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded disabled:opacity-50'
+            disabled={true}
+            onClick={() => {}}>
+            Deposit
+          </button>
+
+          {/* <div> status: {status}</div> */}
+          <div>{error?.message}</div>
+        </div>
+      </div>
+
+      <div className='bg-white rounded-lg shadow p-4 mb-4'>
+        <div className='text-lg font-bold text-gray-400 mb-2'>Rewards</div>
+
+        <div className='grid grid-cols-[85px_1fr] gap-2'>
+          <div className='font-medium'>Refunds:</div>
+          <div>224 credits</div>
+
+          <div className='font-medium'>Referrals:</div>
+          <div>1116 credits</div>
+        </div>
+      </div>
+
+      <div className='bg-white rounded-lg shadow p-4 mb-4'>
+        <div className='text-lg font-bold text-gray-400 mb-2'>Account</div>
         <div className='grid grid-cols-[85px_1fr] gap-2'>
           <div className='font-medium'>Address:</div>
           <div>
